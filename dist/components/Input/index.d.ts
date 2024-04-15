b652d24ffa925a4e53d91ace0d0b8d4dc4291c1b
vikram-hegde <vikramhegde1440@gmail.com>
2024-04-15 09:10:11 +0530

    🚧 build lib with types

---
+++ dist/components/Input/index.d.ts
@@ -0,0 +1,9 @@
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    startIcon?: React.ReactNode;
+    endIcon?: React.ReactNode;
+}
+declare const Input: ({ value, onChange, ...props }: InputProps) => import("react/jsx-runtime").JSX.Element;
+export default Input;
---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { default as Input } from './components/Input';
+import { default as Badge } from './components/Badge';
+import { default as Button } from './components/Button';
+
+export { Button, Badge, Input };
